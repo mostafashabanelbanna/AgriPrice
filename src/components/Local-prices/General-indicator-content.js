ea4bb2e4828c9198a9678808b360cdfa624ad3d5
Mostafa Shaban <moshaban@SH-MOSTAFA-SHAB>
2021-04-12 14:35:10 +0200

    local prices list

---
+++ src/components/Local-prices/General-indicator-content.js
@@ -0,0 +1,321 @@
+import React, { useEffect } from "react";
+import * as moment from "moment";
+
+import PropTypes from "prop-types";
+import { makeStyles, useTheme } from "@material-ui/core/styles";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableFooter from "@material-ui/core/TableFooter";
+import TablePagination from "@material-ui/core/TablePagination";
+import TableRow from "@material-ui/core/TableRow";
+import Paper from "@material-ui/core/Paper";
+import IconButton from "@material-ui/core/IconButton";
+import FirstPageIcon from "@material-ui/icons/FirstPage";
+import KeyboardArrowLeft from "@material-ui/icons/KeyboardArrowLeft";
+import KeyboardArrowRight from "@material-ui/icons/KeyboardArrowRight";
+import LastPageIcon from "@material-ui/icons/LastPage";
+import { TableHead } from "@material-ui/core";
+
+import ArrowDropDownIcon from "@material-ui/icons/ArrowDropDown";
+import ArrowDropUpIcon from "@material-ui/icons/ArrowDropUp";
+import { OverlayTrigger, Tooltip } from "react-bootstrap";
+
+const useStyles1 = makeStyles((theme) => ({
+  root: {
+    flexShrink: 0,
+    marginLeft: theme.spacing(2.5),
+  },
+}));
+
+const useStyles2 = makeStyles({
+  table: {
+    minWidth: 500,
+  },
+});
+
+function TablePaginationActions(props) {
+  const classes = useStyles1();
+  const theme = useTheme();
+  const { count, page, rowsPerPage, onChangePage } = props;
+
+  const handleFirstPageButtonClick = (event) => {
+    onChangePage(event, 0);
+  };
+
+  const handleBackButtonClick = (event) => {
+    onChangePage(event, page - 1);
+  };
+
+  const handleNextButtonClick = (event) => {
+    onChangePage(event, page + 1);
+  };
+
+  const handleLastPageButtonClick = (event) => {
+    onChangePage(event, Math.max(0, Math.ceil(count / rowsPerPage) - 1));
+  };
+
+  return (
+    <div className={classes.root}>
+      <IconButton
+        onClick={handleFirstPageButtonClick}
+        disabled={page === 0}
+        aria-label="first page"
+      >
+        {theme.direction === "rtl" ? <LastPageIcon /> : <FirstPageIcon />}
+      </IconButton>
+      <IconButton
+        onClick={handleBackButtonClick}
+        disabled={page === 0}
+        aria-label="previous page"
+      >
+        {theme.direction === "rtl" ? (
+          <KeyboardArrowRight />
+        ) : (
+          <KeyboardArrowLeft />
+        )}
+      </IconButton>
+      <IconButton
+        onClick={handleNextButtonClick}
+        disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+        aria-label="next page"
+      >
+        {theme.direction === "rtl" ? (
+          <KeyboardArrowLeft />
+        ) : (
+          <KeyboardArrowRight />
+        )}
+      </IconButton>
+      <IconButton
+        onClick={handleLastPageButtonClick}
+        disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+        aria-label="last page"
+      >
+        {theme.direction === "rtl" ? <FirstPageIcon /> : <LastPageIcon />}
+      </IconButton>
+    </div>
+  );
+}
+
+TablePaginationActions.propTypes = {
+  count: PropTypes.number.isRequired,
+  onChangePage: PropTypes.func.isRequired,
+  page: PropTypes.number.isRequired,
+  rowsPerPage: PropTypes.number.isRequired,
+};
+
+const GeneralIndicatorContent = (props) => {
+  const classes = useStyles2();
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(10);
+
+  const emptyRows =
+    rowsPerPage -
+    Math.min(
+      rowsPerPage,
+      props.generalIndicatorDataItem.length - page * rowsPerPage
+    );
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
+  useEffect(() => {
+    //console.log(props.GovId)
+  }, []);
+  const pricePercentageNow = (
+    minPriceWithinYear,
+    maxPriceWithinYear,
+    avgPrice
+  ) => {
+    return (
+      ((avgPrice - minPriceWithinYear) /
+        (maxPriceWithinYear - minPriceWithinYear)) *
+      100
+    );
+  };
+  return (
+    <TableContainer component={Paper}>
+      <Table
+        className={(classes.table, "table  table-responsive")}
+        aria-label="custom pagination table"
+      >
+        <TableHead
+          style={{
+            backgroundColor: "var(--main-green",
+            color: "#fff",
+            textAlign: "center",
+          }}
+        >
+          <TableRow>
+            <TableCell className="text-center"> البيان </TableCell>
+            <TableCell className="text-center "> متوسط السعر </TableCell>
+            <TableCell className="text-center"> قيمة التغير </TableCell>
+            <TableCell className="text-center"> التغير على مدار عام</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {(rowsPerPage > 0
+            ? props.generalIndicatorDataItem.slice(
+                page * rowsPerPage,
+                page * rowsPerPage + rowsPerPage
+              )
+            : props.generalIndicatorDataItem
+          ).map((item, idx) => (
+            <TableRow key={idx}>
+              <TableCell className="text-center" style={{ lineHeight: "2" }}>
+                <span className="mb-5">
+                  {item.indictorName} / {item.unit}
+                </span>
+                <br />
+                <span style={{ color: "rgb(144, 144, 144)" }}>
+                  {moment(item.insertionDate).format("LL")}
+                </span>
+              </TableCell>
+              <TableCell className="text-center d-flex justify-content-center align-items-center border-bottom-0">
+                <span>
+                  {item.avgPrice} {item.currency}
+                </span>
+                <div className="p-2">
+                  <OverlayTrigger
+                    placement={"top"}
+                    overlay={
+                      <Tooltip>
+                        <strong>{item.minSubIndicatorName}</strong>
+                      </Tooltip>
+                    }
+                  >
+                    <div
+                      className="border-bottom"
+                      style={{ color: "var(--main-green)" }}
+                    >
+                      <ArrowDropDownIcon />
+                      أدنى
+                      <span className="mr-2" style={{ color: "#909090" }}>
+                        {item.minPrice} {item.currency}
+                      </span>
+                    </div>
+                  </OverlayTrigger>
+                  <OverlayTrigger
+                    placement={"bottom"}
+                    overlay={
+                      <Tooltip>
+                        <strong>{item.maxSubIndicatorName}</strong>
+                      </Tooltip>
+                    }
+                  >
+                    <div style={{ color: "#FF3232" }}>
+                      <ArrowDropUpIcon />
+                      أعلى
+                      <span n className="mr-2" style={{ color: "#909090" }}>
+                        {item.maxPrice} {item.currency}
+                      </span>
+                    </div>
+                  </OverlayTrigger>
+                </div>
+              </TableCell>
+              <TableCell className="text-center" style={{ lineHeight: "2" }}>
+                {" "}
+                {item.changeRateDaialy > 0 ? (
+                  <span style={{ color: "#FF3232" }}>
+                    +{item.changeRateDaialy}
+                    <br />
+                    {item.changeRatePercentDaialy} %
+                  </span>
+                ) : (
+                  <span style={{ color: "var(--main-green)" }}>
+                    {item.changeRateDaialy}
+                    <br />
+                    {item.changeRatePercentDaialy} %
+                  </span>
+                )}
+              </TableCell>
+              <TableCell className="text-center">
+                <div className="d-flex justify-content-between">
+                  <div style={{ color: "var(--main-green)" }}>أقل سعر</div>
+                  <div style={{ color: "#909090" }}>52 إسبوع</div>
+                  <div style={{ color: "#FF3232" }}>أعلى سعر</div>
+                </div>
+                <div
+                  style={{
+                    height: "16px",
+                    border: "1px solid black",
+                    position: "relative",
+                  }}
+                >
+                  <div
+                    style={{
+                      height: "15px",
+                      borderLeft: `${
+                        pricePercentageNow(
+                          item.minPriceWithinYear,
+                          item.maxPriceWithinYear,
+                          item.avgPrice
+                        ) > 50
+                          ? " 4px solid #FF3232"
+                          : " 4px solid var(--main-green)"
+                      }`,
+                      position: "absolute",
+                      right: `${pricePercentageNow(
+                        item.minPriceWithinYear,
+                        item.maxPriceWithinYear,
+                        item.avgPrice
+                      )}%`,
+                    }}
+                  ></div>
+                </div>
+                <div className="d-flex justify-content-between">
+                  <div>
+                    <div style={{ color: "var(--main-green)" }}>
+                      {item.minPriceWithinYear}
+                    </div>
+                    <div style={{ color: "#909090" }}>
+                      {moment(item.minWithinYearDate).locale("ar").format("LL")}
+                    </div>
+                    {/* {item.minWithinYearDate} */}
+                  </div>
+                  <div>
+                    <div style={{ color: "#FF3232" }}>
+                      {item.maxPriceWithinYear}
+                    </div>
+                    <div style={{ color: "#909090" }}>
+                      {moment(item.maxWithinYearDate).locale("ar").format("LL")}
+                    </div>
+                  </div>
+                </div>
+              </TableCell>
+            </TableRow>
+          ))}
+
+          {/* {emptyRows > 0 && (
+              <TableRow style={{ height: 53 * emptyRows }}>
+                <TableCell colSpan={6} />
+              </TableRow>
+            )} */}
+        </TableBody>
+        <TableFooter>
+          <TableRow>
+            <TablePagination
+              rowsPerPageOptions={[5, 10, 25, { label: "All", value: -1 }]}
+              count={props.generalIndicatorDataItem.length}
+              rowsPerPage={rowsPerPage}
+              page={page}
+              labelRowsPerPage="عدد السلع فى الصفحة"
+              onChangePage={handleChangePage}
+              onChangeRowsPerPage={handleChangeRowsPerPage}
+              ActionsComponent={TablePaginationActions}
+            />
+          </TableRow>
+        </TableFooter>
+      </Table>
+    </TableContainer>
+  );
+};
+
+export default GeneralIndicatorContent;
--- src/components/Local-prices/Local-prices.js
+++ src/components/Local-prices/Local-prices.js
@@ -1,48 +1,106 @@
-import React from "react";
-import Radio from "@material-ui/core/Radio";
-import RadioGroup from "@material-ui/core/RadioGroup";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
-
+import React, { useState, useEffect } from "react";
+import Slider from "react-slick";
 import { Col, Container, Row } from "react-bootstrap";
+import { axios } from "../Axios/Axios";
 
-import RetailPrices from "./Retail-prices";
-import WholesalePrices from "./Wholesale-prices";
-import  "./LocalPrices.css";
+import "./LocalPrices.css";
+import { SampleNextArrow, SamplePrevArrow } from "../slick-carousel/Arrows";
+import GeneralIndicatorTabs from "./General-indicator-tabs";
 
 const LocalPrices = () => {
-  const [value, setValue] = React.useState("retail");
-  const handleRadioChange = (event) => {
-    setValue(event.target.value);
+  const [generalIndicators, setGeneralIndicators] = useState([]);
+  const [generalIndicatorData, setGeneralIndicatorData] = useState();
+  const [currentGeneralIndicator, setCurrentGeneralIndicator] = useState();
+
+  const noGeneralIndicators =
+    !generalIndicators || (generalIndicators && generalIndicators.length === 0); //check if no news
+
+  const getGeneralIndicators = async () => {
+    //fetch generalIndicators data
+    const response = await axios
+      .get("/PricesData/GetGeneralIndicators")
+      .catch((err) => console.log("Error", err)); //handle errors
+    if (response && response.data) {
+      setGeneralIndicators(response.data); // set generalIndicators data to state
+      getGeneralIndicatorData(response.data[0].id); // set GeneralIndicatorData on component mount
+    }
   };
 
+  const getGeneralIndicatorData = async (id) => {
+    const response = await axios
+      .get(`/PricesData/GetGeneralIndicatorAllData/${id}`)
+      .catch((err) => console.log("Error", err)); //handle errors;
+    if (response && response.data) {
+      setGeneralIndicatorData(response.data); // set FocusedGeneralIndicator data to state
+      setCurrentGeneralIndicator(id);
+    }
+  };
+  const handleGeneralIndicatorSelect = (id) => {
+    getGeneralIndicatorData(id); // set GeneralIndicatorData on select lable
+    setCurrentGeneralIndicator(id);
+  };
+
+  useEffect(() => {
+    getGeneralIndicators();
+  }, []);
+
+  // Slider Settings
+  const settings = {
+    className: "center",
+    nextArrow: <SampleNextArrow />,
+    prevArrow: <SamplePrevArrow />,
+    centerMode: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 6,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
+  };
   return (
-    <Container fluid>
-      <Row
-        className="justify-content-center border-bottom py-5"
-        style={{ backgroundColor: "var(--main-gray)" }}
-      >
-        <Col>
-          <RadioGroup
-            className="mb-4"
-            aria-label="market"
-            name="market"
-            value={value}
-            onChange={handleRadioChange}
-            row
-            style={{paddingRight:30}}
-          >
-            <FormControlLabel
-              value="retail"
-              control={<Radio style={{ color: "var(--main-green)" }} />}
-              label="اسواق التجزئة"
-            />
-            <FormControlLabel
-              value="wholesale"
-              control={<Radio style={{ color: "var(--main-green)" }} />}
-              label="اسواق الجملة"
-            />
-          </RadioGroup>
-          {value === "retail" ? <RetailPrices /> : <WholesalePrices />}
+    <Container fluid className="my-4 local-prices">
+      <Row>
+        <Col xs={12}>
+          <h5 style={{ color: "var(--main-green)" }}>المجموعات السلعية</h5>
+        </Col>
+        <Col xs={12} className="my-4">
+          <Slider {...settings}>
+            {!noGeneralIndicators &&
+              generalIndicators.map((Item, idx) => {
+                return (
+                  <div key={Item.id}>
+                    <div
+                      className={`p-2 mx-2 lable ${
+                        Item.id === currentGeneralIndicator ? "active" : ""
+                      }`}
+                      onClick={() => handleGeneralIndicatorSelect(Item.id)}
+                    >
+                      <h6 className="text-center my-1">{Item.nameA}</h6>
+                    </div>
+                  </div>
+                );
+              })}
+          </Slider>
+        </Col>
+        <Col xs={12}>
+          <GeneralIndicatorTabs generalIndicatorData={generalIndicatorData} />
         </Col>
       </Row>
     </Container>
